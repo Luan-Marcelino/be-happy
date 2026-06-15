89e366d5c0e70780a6da755495a4030beccf3137
Luan-Marcelino <luanmarcelinocontato@gmail.com>
2026-06-15 14:07:29 -0300

    Adicionando os produtos

--- JS/script.js
+++ JS/script.js
@@ -22,3 +22,34 @@ window.addEventListener('scroll', () => {
     }
 })
 
+// PRODUTOS
+
+const produtos = [
+    {
+        img: './Imagens/boxes/box1.jpeg',
+        nome: 'Box Azul',
+        valor: '100'
+    },
+    {
+        img: './Imagens/boxes/box2.jpeg',
+        nome: 'Box Marrom',
+        valor: '110'
+    },
+    {
+        img: './Imagens/boxes/box3.jpeg',
+        nome: 'Box Rosa',
+        valor: '120'
+    }
+]
+
+produtos.forEach(produto => {
+    const card = `
+        <div class="card">
+        <img src="${produto.img}" alt="Box 1">
+        <p class="nome-box">${produto.nome}</p>
+        <p class="valor-box">R$${produto.valor},00</p>
+        </div>
+    `
+
+    document.querySelector('#cards').innerHTML += card;
+})
